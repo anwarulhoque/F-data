04987ff4693c2183d3b54ddef556022dc2bc0042
anwarulhoque <anwarul.dk@gmail.com>
2018-03-15 01:34:37 +0600

    My Project Setup compleated successfully.

---
+++ Fusion-backend/src/main/java/net/anw/Fusion_backend/App.java
@@ -0,0 +1,13 @@
+package net.anw.Fusion_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
